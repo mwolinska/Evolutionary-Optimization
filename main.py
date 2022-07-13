190e4674df0562477a5151e980bfb91b50f4dd92
Marta <mswolinska@gmail.com>
2022-07-13 13:11:20 +0200

    Add main script to run package from terminal.

---
+++ main.py
@@ -0,0 +1,16 @@
+from evolution import Evolution
+from genotype import GenotypeKey
+
+
+def run_evolution():
+    test_evolution = Evolution(n_individuals=5,
+                               n_generations=4,
+                               genotype_key=GenotypeKey.a_list,
+                               n_genes=1,
+                               individual_value_range=(-10, 10),
+                               mutation_probability=1,
+                               crossover=True)
+    test_evolution.evolve()
+
+if __name__ == '__main__':
+    run_evolution()
