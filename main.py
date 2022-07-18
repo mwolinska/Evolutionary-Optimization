fba869dd5b1823e48737a09a8513360d59cc2042
Marta <mswolinska@gmail.com>
2022-07-18 15:44:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from genotype.genotype_data_model import GenotypeKey
 
 
 def run_evolution():
-    test_evolution = Evolution(n_individuals=5,
-                               n_generations=4,
-                               genotype_key=list,
-                               type_of_gene=int,
-                               n_genes=1,
-                               gene_value_range=(-10, 10),
-                               mutation_probability=1,
-                               crossover=True)
+    test_evolution = Evolution(
+        n_individuals=5,
+        n_generations=4,
+        genotype_key=list,
+        type_of_gene=int,
+        n_genes=1,
+        gene_value_range=(-10, 10),
+        mutation_probability=1,
+        crossover=True
+    )
+
     test_evolution.evolve()
 
 if __name__ == '__main__':
