a356ed7ecb7ffbc21b643d2205e978864b1d22e0
Marta <mswolinska@gmail.com>
2022-07-18 10:42:15 +0100

    Docstrings for Individual class and optimize imports

--- individual.py
+++ individual.py
@@ -1,26 +1,57 @@
-import random
-from typing import List, Union
-
-import numpy as np
-
 from genotype import Genotype, GenotypeProperties, GenotypeKey
 
 
 class Individual:
     def __init__(self, genotype_properties: GenotypeProperties):
+        """An object containing all information of one instance being evaluated.
+
+        An individual is effectively an instance being evaluated during the evolution. An individual contains
+        a genotype and a fitness score (updated upon evaluation during Evolution).
+
+        Args:
+            genotype_properties: all genotype properties as required for an individual.
+        """
         self.genotype = Genotype(genotype_properties)
+        # TODO the line below is obsolete? Check
         self.genotype.mutation_probability = genotype_properties.mutation_probability
         self.fitness_score = None
 
     @classmethod
-    def from_all_genes(cls, genotype_poroperties, new_all_genes):
+    def from_all_genes(cls, genotype_properties: GenotypeProperties, new_all_genes):
         """Creates an individual from a set of genes.
-        new_individual.genotype.all_genes = new_all_genes
+
+        An individual is created, but instead of a random set of genes the new_all_genes argument
+        is used to populate the individual.all_genes attribute.
+
+            genotype_properties: all genotype properties as required for an individual.
+            new_all_genes: an instance of all_genes in alignment with the GenotypeKey defined by genotype_properties.
+        Returns:
+            An Individual object with the genotype properties passed in the function except for
+                individual.all_genes which contain the new_all_genes information.
+        """
         return new_individual
 
     def mutation(self):
+        """Performs mutation of the individual.
+
+        The genotype attribute is updated through a mutation in line with the function definition within the
+        Genotype class.
+        """
         new_genotype = self.genotype.mutate()
         self.genotype.genotype = new_genotype
+
+def get_score_for_sorting(individual: Individual):
+    """Key for sort function used in Population object.
+
+    Provides a key for sorting individuals using python's sort function used by the update_population function
+    within the Population object.
+
+    Args:
+        individual: an Individual instance.
+    Returns:
+        Float equal to an individual's fitness score.
+    """
+    return individual.fitness_score
 
 if __name__ == '__main__':
     gen_prop = GenotypeProperties(
