c9a109e2f796c5433819c3a1740a5f197a8b2fe1
Marta <mswolinska@gmail.com>
2022-07-29 11:19:16 +0100

    Add docstsrings to AbstractGenotype

--- evolutionary_optimization/genotype/abstract_genotype.py
+++ evolutionary_optimization/genotype/abstract_genotype.py
@@ -12,6 +12,19 @@ class AbstractGenotype(ABC):
         number_of_genes: int,
         value_range: Tuple[int, int],
     ):
+        """Initialise instance of AbstractGenotype.
+
+        Args:
+            genotype: genotype used for mutation, crossover and to calculate phenotype_value.
+            mutation_probability: probability of a gene mutating.
+            ratio_of_population_for_crossover: ratio of population used for crossover when updating population.
+            number_of_genes: number of genes in the genotype.
+            value_range: minimum and maximum values of a gene.
+
+        Todo:
+            * (Marta): How to deal with genotype typing.
+            * (Marta): value range maybe shouldn't be in the constructor
+        """
         pass
 
     @classmethod
@@ -21,13 +34,33 @@ class AbstractGenotype(ABC):
         mutation_probability: Optional[float],
         ratio_of_population_for_crossover: Optional[float],
     ) -> "AbstractGenotype":
+        """Build random genotype attribute based on class parameters.
+
+        Args:
+            mutation_probability: probability of a gene mutating.
+            ratio_of_population_for_crossover: ratio of population used for crossover when updating population.
+
+        Returns:
+            AbstractGenotype object with a randomly generated genotype attribute.
+        """
         pass
 
     @abstractmethod
     def mutate(self):
+        """In place modification of the genotype by randomly changing genes based on mutation probability."""
         pass
 
     @abstractmethod
-    def crossover(self, parent_2: "AbstractGenotype") -> Tuple:
-        # TODO typing
+    def crossover(self, parent_2_genotype: "AbstractGenotype") -> Tuple["AbstractGenotype", "AbstractGenotype"]:
+        """Perform crossover between two phenotypes.
+
+        Combines a portion of this object's genotype with that of parent_2 to return 2 new phenotypes
+        based on the combined genotypes. The new genotype length is the same as of the parents.
+
+        Args:
+            parent_2_genotype: a genotype of the same class whose genotype will be mixed with
+
+        Returns:
+            Two new phenotype instances based on the combined genotypes of the two parents.
+        """
         pass
