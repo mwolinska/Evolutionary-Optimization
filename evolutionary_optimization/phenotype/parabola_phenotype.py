a6b6eb4ad75e0b753b9068c0559c00517a46a163
Marta <mswolinska@gmail.com>
2022-07-29 10:43:26 +0100

    Add docstrings for ParabolaPhenotype and fix missing typing

--- evolutionary_optimization/phenotype/parabola_phenotype.py
+++ evolutionary_optimization/phenotype/parabola_phenotype.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Tuple
 
 from evolutionary_optimization.genotype.binary_list_genotype import BinaryListGenotype
 from evolutionary_optimization.genotype.integer_list_genotype import IntegerListGenotype
@@ -7,19 +7,27 @@ from evolutionary_optimization.phenotype.abstract_phenotype import AbstractPheno
 
 class ParabolaPhenotype(AbstractPhenotype):
     def __init__(self, genotype: Union[BinaryListGenotype, IntegerListGenotype]):
+        """Initialise InvertedParabolaPhenotype object.
+
+        Args
+            genotype: an AbstractGenotype that defines the phenotype.
+        """
         self.genotype = genotype
         self._phenotype_value = None
 
     @property
     def phenotype_value(self):
+        """Stores value of the phenotype based on the genotype - calculated using evaluate_phenotype."""
         return self._phenotype_value
 
     @phenotype_value.setter
     def phenotype_value(self, value: int):
+        """Setter for phenotype_value property."""
         self._phenotype_value = value
 
     @classmethod
     def from_phenotype(cls, base_phenotype: "ParabolaPhenotype") -> "ParabolaPhenotype":
+        """Create new phenotype with the same attributes as the base phenotype, but a new random genotype.genotype."""
         new_genotype = base_phenotype.genotype.build_random_genotype(
             number_of_genes=base_phenotype.genotype.number_of_genes,
             value_range=base_phenotype.genotype.value_range,
@@ -29,6 +37,12 @@ class ParabolaPhenotype(AbstractPhenotype):
         return cls(new_genotype)
 
     def evaluate_phenotype(self):
+        """In place method to calculate phenotype value using genotype.
+
+        This phenotype follows x^2 and as such is a single parameter optimisation problem.
+        For a binary genotype the integer value is returned,for an integer list genotype only the first value is used.
+        It updates the phenotype_value property in place once the calculation is done.
+        """
         if isinstance(self.genotype, BinaryListGenotype):
             float_genotype = self.genotype.return_integer_form()
         else:
@@ -37,11 +51,28 @@ class ParabolaPhenotype(AbstractPhenotype):
         phenotype = float_genotype ** 2
         self.phenotype_value = phenotype
 
-    def crossover(self, parent_2: "ParabolaPhenotype"):
+    def crossover(self, parent_2: "ParabolaPhenotype") -> Tuple["ParabolaPhenotype", "ParabolaPhenotype"]:
+        """Perform crossover between two phenotypes.
+
+        Calls crossover method from the genotype attribute. Combines a portion of this object's genotype
+        with that of parent_2 to return 2 new phenotypes based on the combined genotypes.
+        The new genotype length is the same as of the parents.
+
+        Args:
+            parent_2: a phenotype of the same class whose genotype will be mixed with
+
+        Returns:
+            Two new phenotype instances based on the combined genotypes of the two parents.
+        """
         child_genotype_1, child_genotype_2 = self.genotype.crossover(parent_2.genotype)
         child_1 = ParabolaPhenotype(child_genotype_1)
         child_2 = ParabolaPhenotype(child_genotype_2)
         return child_1, child_2
 
     def mutate(self):
+        """In place modification of the genotype by randomly changing genes based on mutation probability.
+
+        Calls mutate method as implemented for the genotype attribute in order to perform mutation.
+        Updates genotype attribute in place.
+        """
         self.genotype.mutate()
