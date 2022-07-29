7591bda7d68410eb2465ad3abf34872edbf9d31d
Marta <mswolinska@gmail.com>
2022-07-29 11:53:41 +0100

    Changing _create_population to rely only on self

--- evolutionary_optimization/population.py
+++ evolutionary_optimization/population.py
@@ -25,14 +25,14 @@ class Population:
         """
 
         self.number_of_individuals = number_of_individuals
-        self.population = self._create_population(phenotype)
         self.phenotype = phenotype
+        self.population = self._create_population()
         self.best_individual = phenotype.from_phenotype(phenotype)
         self.mutation = True if phenotype.genotype.mutation_probability > 0 else False
         self.crossover = True if phenotype.genotype.mutation_probability > 0 else False
         self.ratio_of_elite_individuals = ratio_of_elite_individuals
 
-    def _create_population(self, phenotype) -> List[AbstractPhenotype]:
+    def _create_population(self) -> List[AbstractPhenotype]:
         """Create initial population of individuals.
 
         Returns:
@@ -40,7 +40,7 @@ class Population:
         """
         individuals = []
         for i in range(self.number_of_individuals):
-            new_individual = phenotype.from_phenotype(phenotype)
+            new_individual = self.phenotype.from_phenotype(self.phenotype)
             individuals.append(new_individual)
         return individuals
 
