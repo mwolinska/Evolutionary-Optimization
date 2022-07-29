d149195b9d6b3e66731f151c537b0041064e3abd
Marta <mswolinska@gmail.com>
2022-07-29 11:39:10 +0100

    Updating main script

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from evolutionary_optimization.evolution import Evolution
-from fitness_functions.fitness_interface import FitnessFunctions
-from genotype.genotype_interface import Genotype, Genotypes
-from phenotype.phenotype_interface import Phenotype, Phenotypes
+from evolutionary_optimization.fitness_functions.fitness_interface import FitnessFunctions
+from evolutionary_optimization.genotype.genotype_interface import Genotype, Genotypes
+from evolutionary_optimization.phenotype.phenotype_interface import Phenotype, Phenotypes
 
-if __name__ == '__main__':
-    genotype_class = Genotype().get_genotype(Genotypes.BINARY_LIST)
-    phenotype_class = Phenotype().get_phenotype(Phenotypes.PARABOLA)
+
+def run():
+    genotype_class = Genotype.get_genotype(Genotypes.BINARY_LIST)
+    phenotype_class = Phenotype.get_phenotype(Phenotypes.PARABOLA)
 
     evolutionary_algorithm = Evolution(phenotype=phenotype_class(genotype_class()),
                                        number_of_individuals=100,
@@ -13,3 +14,8 @@ if __name__ == '__main__':
                                        fitness_function=FitnessFunctions.MINIMIZE
                                        )
     evolutionary_algorithm.evolve()
+    evolutionary_algorithm.plot_performance()
+
+
+if __name__ == '__main__':
+   run()
