f616cd7e8ead50562b3c68b9fd9dd0410fff3da2
Marta <mswolinska@gmail.com>
2022-08-02 12:12:58 +0100

    Update crossover methods in existing classes

--- evolutionary_optimization/genotype/binary_list_genotype.py
+++ evolutionary_optimization/genotype/binary_list_genotype.py
@@ -4,6 +4,7 @@ from typing import Tuple, Optional, List
 import numpy as np
 
 from evolutionary_optimization.genotype.abstract_genotype import AbstractGenotype
+from evolutionary_optimization.genotype.genotype_utils import single_point_crossover
 
 
 class BinaryListGenotype(AbstractGenotype):
@@ -115,8 +116,8 @@ class BinaryListGenotype(AbstractGenotype):
             last_slice_index = self.number_of_genes - 1
             gene_slice_index = randint(1, last_slice_index)
 
-            child_1_genotype = self.single_point_crossover(self.genotype, parent_2_genotype.genotype, gene_slice_index)
-            child_2_genotype = self.single_point_crossover(parent_2_genotype.genotype, self.genotype, gene_slice_index)
+            child_1_genotype = single_point_crossover(self.genotype, parent_2_genotype.genotype, gene_slice_index)
+            child_2_genotype = single_point_crossover(parent_2_genotype.genotype, self.genotype, gene_slice_index)
 
             child_1 = BinaryListGenotype(
                 genotype=child_1_genotype,
@@ -135,31 +136,6 @@ class BinaryListGenotype(AbstractGenotype):
             )
 
             return child_1, child_2
-
-    @staticmethod
-    def single_point_crossover(
-        parent_1_genotype: List[int],
-        parent_2_genotype: List[int],
-        gene_slice_index: int,
-    ) -> List[int]:
-        """A single point crossover for genotype of type list.
-
-        This is a single point crossover. Using the gene_slice_index, for both parents the genotype are sliced.
-        The slice [:gene_slice_index] is taken from parent_1 and the slice [gene_slice_index:] is taken from parent_2.
-        The two complementary slices are then joined to create a new genotype (a child genotype).
-
-        Args:
-            parent_1_genotype: genotype which will be used to create an offspring.
-            parent_2_genotype: genotype which will be used to create an offspring.
-            gene_slice_index: random integer at which the parent genotypes will be sliced.
-
-        Returns:
-            A genotype used to build a Genotype object.
-        """
-        child_genotype_part_1 = parent_1_genotype[:gene_slice_index]
-        child_genotype_part_2 = parent_2_genotype[gene_slice_index:]
-        child_genotype = child_genotype_part_1 + child_genotype_part_2
-        return child_genotype
 
     def return_integer_form(self):
         """Return integer form of a binary number."""
--- evolutionary_optimization/genotype/integer_list_genotype.py
+++ evolutionary_optimization/genotype/integer_list_genotype.py
@@ -4,6 +4,7 @@ from typing import Tuple, Optional, List
 import numpy as np
 
 from evolutionary_optimization.genotype.abstract_genotype import AbstractGenotype
+from evolutionary_optimization.genotype.genotype_utils import single_point_crossover
 
 
 class IntegerListGenotype(AbstractGenotype):
@@ -117,8 +118,8 @@ class IntegerListGenotype(AbstractGenotype):
             last_slice_index = self.number_of_genes - 1
             gene_slice_index = randint(1, last_slice_index)
 
-            child_1_genotype = self.single_point_crossover(self.genotype, parent_2_genotype.genotype, gene_slice_index)
-            child_2_genotype = self.single_point_crossover(parent_2_genotype.genotype, self.genotype, gene_slice_index)
+            child_1_genotype = single_point_crossover(self.genotype, parent_2_genotype.genotype, gene_slice_index)
+            child_2_genotype = single_point_crossover(parent_2_genotype.genotype, self.genotype, gene_slice_index)
 
             child_1 = IntegerListGenotype(genotype=child_1_genotype,
                                           mutation_probability=self.mutation_probability,
@@ -134,28 +135,3 @@ class IntegerListGenotype(AbstractGenotype):
             # TODO (Marta): implement method to return Genotype copy with updated genotype attribuet
 
             return child_1, child_2
-
-    @staticmethod
-    def single_point_crossover(
-        parent_1_genotype: List[int],
-        parent_2_genotype: List[int],
-        gene_slice_index: int,
-    ) -> List[int]:
-        """A single point crossover for genotype of type list.
-
-        This is a single point crossover. Using the gene_slice_index, for both parents the genotype are sliced.
-        The slice [:gene_slice_index] is taken from parent_1 and the slice [gene_slice_index:] is taken from parent_2.
-        The two complementary slices are then joined to create a new genotype (a child genotype).
-
-        Args:
-            parent_1_genotype: genotype which will be used to create an offspring.
-            parent_2_genotype: genotype which will be used to create an offspring.
-            gene_slice_index: random integer at which the parent genotypes will be sliced.
-
-        Returns:
-            A genotype used to build a Genotype object.
-        """
-        child_genotype_part_1 = parent_1_genotype[:gene_slice_index]
-        child_genotype_part_2 = parent_2_genotype[gene_slice_index:]
-        child_genotype = child_genotype_part_1 + child_genotype_part_2
-        return child_genotype
