de7e6a939c83409356a028b5228b257f42bbc528
Marta <mswolinska@gmail.com>
2022-08-04 19:10:58 +0100

    Update generate_points_for_function to handle 2D and 3D data

--- evolutionary_optimization/phenotype/phenotype_model/phenotype_utils.py
+++ evolutionary_optimization/phenotype/phenotype_model/phenotype_utils.py
@@ -1,6 +1,8 @@
-from typing import Union
+from dataclasses import dataclass
+from typing import Union, List, Optional
 
 import numpy as np
+from numpy import ndarray
 
 from evolutionary_optimization.phenotype import AbstractPhenotype
 
@@ -10,22 +12,33 @@ class PlottingData:
     y: ndarray
     z: Optional[ndarray] = None
 
-def generate_points_for_function(
-        phenotype: AbstractPhenotype,
-        bottom_plotting_limit: Union[float, int] = -50,
-        upper_plotting_limit: Union[float, int] = 50,
-        number_of_points: int = 100,
-    ):
-    x_values_for_plot = []
-    y_values_for_plot = []
 
+def generate_points_for_function(
+    phenotype: AbstractPhenotype,
+    bottom_plotting_limit: Union[float, int] = -50,
+    upper_plotting_limit: Union[float, int] = 50,
+    number_of_points: int = 100,
+    three_dimensions: bool = False
+        # TODO (Marta): remove three_dimensions as a parameter
+) -> PlottingData:
+
+    function_data = PlottingData(x=np.empty(0), y=np.empty(0))
     range_of_x = np.linspace(bottom_plotting_limit, upper_plotting_limit, num=number_of_points)
 
-    for x in range_of_x:
-        x_values_for_plot.append(x)
-        phenotype.genotype.genotype = [x]
-        phenotype.evaluate_phenotype()
-        y_value = phenotype.phenotype_value
-        y_values_for_plot.append(y_value)
+    if not three_dimensions:
+        for x_datapoint in range_of_x:
+            function_data.x = np.append(function_data.x, x_datapoint)
+            phenotype.genotype.genotype = [x_datapoint]
+            phenotype.evaluate_phenotype()
+            function_data.y = np.append(function_data.x, phenotype.phenotype_value)
 
-    return x_values_for_plot, y_values_for_plot
+    if three_dimensions:
+        function_data.z = np.empty(0)
+
+        x_mesh, y_mesh = np.meshgrid(np.asarray(range_of_x), np.asarray(range_of_x))
+        z_mesh = phenotype.evaluate_phenotype_using_arrays(x_mesh, y_mesh)
+        function_data.x = x_mesh
+        function_data.y = y_mesh
+        function_data.z = z_mesh
+
+    return function_data
