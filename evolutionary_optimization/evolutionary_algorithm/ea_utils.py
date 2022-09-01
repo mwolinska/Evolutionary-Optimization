bc6e5ac2f2ef2650affb9722102281769b361e08
Marta <mswolinska@gmail.com>
2022-09-01 14:52:18 +0100

    Refactor CreateGif to differentiate between 2D and 3D

--- evolutionary_optimization/evolutionary_algorithm/ea_utils.py
+++ evolutionary_optimization/evolutionary_algorithm/ea_utils.py
@@ -1,24 +1,27 @@
 import numpy as np
-from matplotlib import animation, pyplot as plt, lines
+from matplotlib import animation, pyplot as plt, lines, cm
+
+from evolutionary_optimization.phenotype.phenotype_model.phenotype_utils import PlottingData
 
 
-class CreateGif:
-    def __init__(self, animation_data_x: np.ndarray, animation_data_y: np.ndarray, static_plot_data: np.ndarray):
-        self.fig = plt.figure()
-        self.ax = plt.axes(
-            xlim=(np.min(static_plot_data[0]), np.max(static_plot_data[0])),
-            ylim=(np.min(static_plot_data[1]), np.max(static_plot_data[1])),
-        )
+class CreateGif2D:
+    def __init__(self, animation_data_x: np.ndarray, animation_data_y: np.ndarray, static_plot_data: PlottingData):
+        self.fig, self.ax = plt.subplots()
+
+        self.ax.set_xlim((np.min(static_plot_data.x), np.max(static_plot_data.x)))
+        self.ax.set_ylim((np.min(static_plot_data.y), np.max(static_plot_data.y)))
+
         self.time_text = self.ax.text(0.02, 0.95, '', transform=self.ax.transAxes)
         self.best_genotype_text = self.ax.text(0.02, 0.90, '', transform=self.ax.transAxes)
         self.best_phenotype_text = self.ax.text(0.02, 0.85, '', transform=self.ax.transAxes)
         self.line, = self.ax.plot([], [], "ro")
+
         self.x_data = animation_data_x
         self.y_data = animation_data_y
         self.x = []
         self.y = []
-        self.frame_counter = 0
-        self.ax.plot(static_plot_data[0], static_plot_data[1])
+
+        self.ax.plot(static_plot_data.x, static_plot_data.y)
 
     def plot_background_for_frame(self):
         self.line.set_data([], [])
