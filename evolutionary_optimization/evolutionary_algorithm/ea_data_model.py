af8761c06a26b6e2fced00dfb6c3d30ea802bf82
Marta <mswolinska@gmail.com>
2022-09-09 16:59:16 +0100

    Update ea_data_model documentation

--- evolutionary_optimization/evolutionary_algorithm/ea_data_model.py
+++ evolutionary_optimization/evolutionary_algorithm/ea_data_model.py
@@ -4,6 +4,7 @@ from typing import Union, List
 
 @dataclass
 class PerformancePlotting:
+    """Dataclass storing information about the algorithm's state overtime."""
     fitness_over_time: List[Union[float, int]]
     phenotype_over_time: List[Union[float, int]]
     genotype_over_time: List[Union[float, int, List[Union[float, int]]]]
